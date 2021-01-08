9d692bf377a8c24987dfe1072d33ed5efd4797ee
puzzud <puzzud@gmail.com>
2021-01-08 00:17:16 -0500

    Implemented timer to execute 60hz drawing and other functionality in msdos.

--- project/src/puzl/msdos/core.c
+++ project/src/puzl/msdos/core.c
@@ -3,22 +3,42 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <conio.h>
+#include <dos.h>
 
 #include <puzl.h>
 
+#define TIMER_COUNTER_0 0x40  // Counter 0.
+
+#define TIMER_CONTROL   0x43  // The 8253's control register.
+#define TIMER_SET_BITS  0x3c  // The bit pattern that will place the timer into
+                              // binary counting with counter load sequence of
+                              // low byte then high byte.
+
+#define TIMER_120HZ 0x26d7 // 120 hz
+#define TIMER_60HZ  0x4dae // 60 hz
+#define WAIT_TIME 3
+
 byte Running;
+
+void _WCINTERRUPT (FAR* OldTimerIsr)(void); // Holds old timer interrupt handler.
 
 void Initialize(void);
 void Shutdown(void);
 void MainLoop(void);
 
+void TimerProgram(int timer, unsigned int rate);
+long GetTime(void);
+
+void InitializeFrameInterrupt(void);
+void ShutdownFrameInterrupt(void);
+void _WCINTERRUPT FAR FrameInterrupt(void);
+
 int main(int argc, char* args[])
 {
 	Initialize();
-
 	MainLoop();
-
-	ShutdownVideo();
+	Shutdown();
 
 	return 0;
 }
@@ -32,24 +52,88 @@ void Initialize(void)
 
 	InitializeNodeTree();
 
-	Running = 1;
+	InitializeFrameInterrupt();
 }
 
 void Shutdown(void)
 {
+	ShutdownFrameInterrupt();
+
 	ShutdownVideo();
-	//ShutdownInput();
+	ShutdownInput();
+}
+
+void InitializeFrameInterrupt(void)
+{
+	// Save old timekeeper ISR.
+	OldTimerIsr = _dos_getvect(0x1c);
+
+	// Install our new ISR.
+	_dos_setvect(0x1c, FrameInterrupt);
+
+	TimerProgram(TIMER_COUNTER_0, TIMER_60HZ);
+}
+
+void ShutdownFrameInterrupt(void)
+{
+	_dos_setvect(0x1c, OldTimerIsr);
 }
 
 inline void MainLoop(void)
 {
+	long startTime = GetTime();
+	long nextTime = startTime + 1;
+
+	Running = 1;
+
 	while (Running != 0)
 	{
 		ProcessInput();
 
 		Process();
 
-		Draw();
-		//ProcessSequences();
+		while (GetTime() < nextTime);
+
+		//Draw();
+
+		startTime = nextTime;
+		++nextTime;
 	}
+}
+
+void _WCINTERRUPT FAR FrameInterrupt(void)
+{
+	//_asm cli;
+
+	if (Running == 0)
+	{
+		return;
+	}
+
+	Draw();
+	//ProcessSequences();
+
+	//_asm sti;
+
+	_chain_intr(OldTimerIsr);
+}
+
+void TimerProgram(int timer, unsigned int rate)
+{
+	// Re-program the internal timer.
+
+	// First program the timer to mode 2 - binary and data loading sequence of
+	// low byte then high byte.
+	outp(TIMER_CONTROL, TIMER_SET_BITS);
+
+	// Write least significant byte of the new rate to the counter register.
+	outp(timer, LowByte(rate));
+
+	// And now the the most significant byte.
+	outp(timer, HighByte(rate));
+}
+
+long GetTime(void)
+{
+	return *(long FAR*)0x0000046cl;
 }

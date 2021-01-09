23bc3b0e8063e86288b4888f0239d1e66c49d0aa
puzzud <puzzud@gmail.com>
2021-01-08 23:39:33 -0500

    Tilemaps are now drawn in msdos.

--- project/src/puzl/msdos/tilemap.c
+++ project/src/puzl/msdos/tilemap.c
@@ -9,11 +9,13 @@ byte TileMapShapeCodes[TILEMAP_WIDTH * TILEMAP_HEIGHT];
 byte TileMapColorCodes[TILEMAP_WIDTH * TILEMAP_HEIGHT];
 byte TileMapCollisionCodes[TILEMAP_WIDTH * TILEMAP_HEIGHT];
 
+extern const byte CharacterSet[NUMBER_OF_CHARACTERS][CHARACTER_HEIGHT];
+
 byte PrintX;
 byte PrintY;
 byte PrintColor;
 
-void DrawCharacter(unsigned int x, unsigned int y, unsigned int width, unsigned int height, byte shapeCode, byte colorCode);
+void DrawCharacter(unsigned int x, unsigned int y, byte shapeCode, byte colorCode);
 
 void InitializeTilemap(void)
 {
@@ -22,12 +24,45 @@ void InitializeTilemap(void)
 	memset(TileMapCollisionCodes, 0, sizeof(TileMapCollisionCodes));
 }
 
-void DrawCharacter(unsigned int x, unsigned int y, unsigned int width, unsigned int height, byte shapeCode, byte colorCode)
+void DrawCharacter(unsigned int x, unsigned int y, byte shapeCode, byte colorCode)
 {
-	if (shapeCode != 0)
+	const byte* characterOffset = &CharacterSet[shapeCode];
+	byte FAR* videoBufferOffset = &DoubleBuffer[(y * SCREEN_WIDTH) + x];
+
+	int yCounter = TILE_HEIGHT;
+	int xCounter;
+	byte rowPixels;
+
+	do
 	{
-		DrawRectangle(x, y, width, height, colorCode);
+		xCounter = TILE_WIDTH;
+
+		rowPixels = *characterOffset;
+		if (rowPixels != 0)
+		{
+			do
+			{
+				if ((rowPixels & 0x80) != 0)
+				{
+					*videoBufferOffset = colorCode;
+				}
+
+				++videoBufferOffset;
+
+				rowPixels <<= 1;
+			}
+			while (--xCounter != 0);
+
+			videoBufferOffset += SCREEN_WIDTH - TILE_WIDTH;
+		}
+		else
+		{
+			videoBufferOffset += SCREEN_WIDTH;
+		}
+
+		++characterOffset;
 	}
+	while (--yCounter != 0);
 }
 
 void DrawTileMap()
@@ -46,7 +81,7 @@ void DrawTileMap()
 			if (shapeCode != 0)
 			{
 				colorCode = TileMapColorCodes[(rowIndex * TILEMAP_WIDTH) + columnIndex];
-				DrawCharacter(columnIndex * TILE_WIDTH, rowIndex * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT, shapeCode, colorCode);
+				DrawCharacter(columnIndex * TILE_WIDTH, rowIndex * TILE_HEIGHT, shapeCode, colorCode);
 			}
 		}
 	}

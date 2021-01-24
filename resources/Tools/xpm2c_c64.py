679014cd14b0801adeab0e1a01e41a6d892b1c6e
puzzud <puzzud@gmail.com>
2021-01-24 00:48:34 -0500

    Implemented xpm2c c64 character set.

--- resources/Tools/xpm2c_c64.py
+++ resources/Tools/xpm2c_c64.py
@@ -9,9 +9,11 @@ class Xpm2CFacility:
 		if type == "chr":
 			self.frameWidth = 8
 			self.frameHeight = 8
+			self.bytesPerRow = 1
 		elif self.type == "spr":
 			self.frameWidth = 16
 			self.frameHeight = 16
+			self.bytesPerRow = 3
 
 	def GenerateFrameRowsFromXpm(self, xpmData, x, y):
 		pixelX = (x * self.frameWidth) + x + 1
@@ -20,45 +22,67 @@ class Xpm2CFacility:
 		frameRows = []
 		for rowIndex in range(0, self.frameHeight):
 			rowString = xpmData.imageData.rows[pixelY + rowIndex][pixelX:pixelX + self.frameWidth]
-			frameRows += self.GenerateRowFromRowString(rowString, xpmData.headerData.palette)
+			row = self.GenerateRowFromRowString(rowString, xpmData.headerData.palette)
+			frameRows.append(row)
 
 		if self.type == "spr":
 			# 21 C64 sprite height.
 			for rowIndex in range(21 - self.frameHeight):
 				row = [0, 0, 0]
-				frameRows += row
+				frameRows.append(row)
 				
-				frameRows.append(0) # Last unused byte.
-
+			frameRows.append([0]) # Last unused byte.
+		
 		return frameRows
 
 	def GenerateRowFromRowString(self, rowString, xpmPalette):
 		row = []
-
-		rowString += "".zfill(24 - len(rowString)).replace('0', 'o')
-
+		
+		# TODO: Need to ensure '0' to 'o' mapping is correct.
+		# Probably not always the case.
+		rowString += "".zfill((self.bytesPerRow * 8) - len(rowString)).replace('0', 'o')
+		
 		while len(rowString) > 0:
-			rowStringByte = rowString[0:8]
-			row.append(self.GetByteFromRowStringByte(rowStringByte, xpmPalette))
+			rowStringOctet = rowString[0:8]
+			row.append(self.GetByteFromRowOctet(rowStringOctet, xpmPalette))
 
 			rowString = rowString[8:len(rowString)]
-
+		
 		return row
 
-	def GetByteFromRowStringByte(self, rowStringByte, xpmPalette):
+	def GetByteFromRowOctet(self, rowStringOctet, xpmPalette):
 		byte = 0x00
 		
-		while len(rowStringByte) > 0:
-			rowStringPair = rowStringByte[0:2]
+		if self.type == "chr":
+			# NOTE: Assumes monochrome.
+			byte = self.GetByteFromRowOctetMonochrome(rowStringOctet, xpmPalette)
+		elif self.type == "spr":
+			# NOTE: Assumes multicolor.
+			while len(rowStringOctet) > 0:
+				rowStringPair = rowStringOctet[0:2]
 
-			byte <<= 2
-			byte += (self.GetByteFromRowStringPair(rowStringPair, xpmPalette))
+				byte <<= 2
+				byte += self.GetByteFromRowStringPairMultiColor(rowStringPair, xpmPalette)
 
-			rowStringByte = rowStringByte[2:len(rowStringByte)]
+				rowStringOctet = rowStringOctet[2:len(rowStringOctet)]
 		
 		return byte
 
-	def GetByteFromRowStringPair(self, rowStringPair, xpmPalette):
+	def GetByteFromRowOctetMonochrome(self, rowStringOctet, xpmPalette):
+		byte = 0x00
+		
+		for i in range(8):
+			colorSymbol = rowStringOctet[i]
+			colorCode = xpmPalette.colorSymbols[colorSymbol]
+			if colorCode != 0:
+				colorCode = 1
+			
+			byte <<= 1
+			byte |= colorCode
+
+		return byte
+
+	def GetByteFromRowStringPairMultiColor(self, rowStringPair, xpmPalette):
 		colorCodeToCrumb = {
 			0: 0x00,
 			1: 0x01,
@@ -81,15 +105,8 @@ class Xpm2CFacility:
 			numberOfRows = 21
 
 		for i in range(0, numberOfRows):
-			i = i # pylint.
+			row = frame[i]
 
-			row = []
-			if len(frame) > 0:
-				row = frame[0:3]
-				frame = frame[3:len(frame) - 1]
-			else:
-				row = [0, 0, 0]
-			
 			previewString = self.GetPreviewStringFromRow(row)
 
 			sys.stdout.write("\t\t")
@@ -99,15 +116,19 @@ class Xpm2CFacility:
 			line += " // " + previewString
 			print(line)
 
-		sys.stdout.write("\t\t")
+		sys.stdout.write("\t")
 
 		if self.type == "spr":
+			sys.stdout.write("\t")
+
 			line = "0"
 			line += "".zfill(minimumLineLength - len(line)).replace('0', ' ')
 			line += " // " + "Unused byte."
 			print(line)
 
 			sys.stdout.write("\t}")
+		else:
+			sys.stdout.write("}")
 
 	def GetPreviewStringFromRow(self, row):
 		previewString = ""

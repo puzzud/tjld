d1d4a694ce8919ce2e27cdb3bc63f6e82bb7075a
puzzud <puzzud@gmail.com>
2021-07-04 19:23:28 -0400

    Added starts of MSX include files.

---
+++ project/src/puzl/msx/msx.asm
@@ -0,0 +1,34 @@
+;
+; MSX generic definitions.
+
+; ---------------------------------------------------------------------------
+; Screen size
+
+defc SCREEN_WIDTH  = 256
+defc SCREEN_HEIGHT = 192
+
+; ---------------------------------------------------------------------------
+; MSX system variables
+defc FORCLR = $f3e9 ; Foreground color
+defc BAKCLR = $f3eA ; Background color
+defc BDRCLR = $f3eB ; Border color
+
+; ---------------------------------------------------------------------------
+; MSX BIOS routine addresses
+; TODO: Incomplete.
+defc CHGMOD = $005f
+defc CHGCLR = $0062
+defc SETTXT = $0078
+defc SETGRP = $007e
+defc CALPAT = $0084
+defc CALATR = $0087
+defc GSPSIZ = $008a
+defc GRPPRT = $008d
+defc CHSNS = $009c
+defc CHGET = $009f
+defc CHPUT = $00a2
+defc BEEP = $00c0
+defc CLS = $00c3
+defc POSIT = $00c6
+defc GTSTCK = $00d5
+defc GTTRIG = $00d8
---
+++ project/src/puzl/msx/msx.h
@@ -0,0 +1,34 @@
+//
+// MSX generic definitions.
+
+// ---------------------------------------------------------------------------
+// Screen size
+
+#define SCREEN_WIDTH  256
+#define SCREEN_HEIGHT 192
+
+// ---------------------------------------------------------------------------
+// MSX system variables
+#define FORCLR = 0xf3e9 // Foreground color
+#define BAKCLR = 0xf3eA // Background color
+#define BDRCLR = 0xf3eB // Border color
+
+// ---------------------------------------------------------------------------
+// MSX BIOS routine addresses
+// TODO: Incomplete.
+#define CHGMOD 0x005f
+#define CHGCLR 0x0062
+#define SETTXT 0x0078
+#define SETGRP 0x007e
+#define CALPAT 0x0084
+#define CALATR 0x0087
+#define GSPSIZ 0x008a
+#define GRPPRT 0x008d
+#define CHSNS 0x009c
+#define CHGET 0x009f
+#define CHPUT 0x00a2
+#define BEEP 0x00c0
+#define CLS 0x00c3
+#define POSIT 0x00c6
+#define GTSTCK 0x00d5
+#define GTTRIG 0x00d8

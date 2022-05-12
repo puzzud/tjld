3fa8452b111e044e8e6028811f6a21de2c756ad1
puzzud <puzzud@gmail.com>
2022-05-11 23:38:04 -0400

    Fixed puzl.h if and endif preprocessor blocks to ensure OpenWatcom configuration is executed correctly.

--- project/src/puzl/puzl.h
+++ project/src/puzl/puzl.h
@@ -1,96 +1,94 @@
 #ifndef PUZL_H
-#define PUZL_H
+	#define PUZL_H
 
-// NOTE: Bad hack to get VS Code to shut up about cc65
-// keywords that are not ISO.
-#ifdef __CC65__
-#define FASTCALL __fastcall__
-#else
-#define FASTCALL
-#endif
+	// NOTE: Bad hack to get VS Code to shut up about cc65
+	// keywords that are not ISO.
+	#ifdef __CC65__
+		#define FASTCALL __fastcall__
+	#else
+		#define FASTCALL
+	#endif
 
-// NOTE: Bad hack to get VS Code to shut up about far
-// keyword (not ISO?).
-#ifdef __WATCOM__
-#define FAR far
-#else
-#define FAR
-#endif
+	// NOTE: Bad hack to get VS Code to shut up about far
+	// keyword (not ISO?).
+	#ifdef __WATCOM__
+		#define FAR far
+	#else
+		#define FAR
+	#endif
 
-#define SetBits(x,bits)   (x | bits)
-#define ClearBits(x,bits) (x & ~bits)
+	#define SetBits(x,bits)   (x | bits)
+	#define ClearBits(x,bits) (x & ~bits)
 
-#define LowByte(n)  (n & 0x00ff)
-#define HighByte(n) ((n>>8) & 0x00ff)
+	#define LowByte(n)  (n & 0x00ff)
+	#define HighByte(n) ((n>>8) & 0x00ff)
 
-typedef unsigned char byte;
-typedef unsigned short word;
+	typedef unsigned char byte;
+	typedef unsigned short word;
 
-#if defined(__NULL__)
-#define HAS_KEYBOARD
+	#if defined(__NULL__)
+		#define HAS_KEYBOARD
 
-#include <null/core.h>
-#include <null/video.h>
-#include <null/color.h>
-#include <null/input.h>
-#include <null/audio.h>
-#else
+		#include <null/core.h>
+		#include <null/video.h>
+		#include <null/color.h>
+		#include <null/input.h>
+		#include <null/audio.h>
+	#else
 
-#if defined(__linux__) || defined(_WIN32) | defined(__EMSCRIPTEN__)
-#define HAS_KEYBOARD
+		#if defined(__linux__) || defined(_WIN32) | defined(__EMSCRIPTEN__)
+			#define HAS_KEYBOARD
 
-#include <sdl/core.h>
-#include <sdl/video.h>
-#include <sdl/color.h>
-#include <sdl/input.h>
-#include <sdl/audio.h>
-#else
+			#include <sdl/core.h>
+			#include <sdl/video.h>
+			#include <sdl/color.h>
+			#include <sdl/input.h>
+			#include <sdl/audio.h>
+		#else
 
-#ifdef __CC65__
+			#ifdef __CC65__
+				#ifdef __C64__
+					#define HAS_KEYBOARD
 
-#ifdef __C64__
-#define HAS_KEYBOARD
+					#pragma rodata-name ("RODATA")
 
-#pragma rodata-name ("RODATA")
+					#include <c64/core.h>
+					#include <c64/video.h>
+					#include <c64/input.h>
+					#include <audio.h>
+				#else
 
-#include <c64/core.h>
-#include <c64/video.h>
-#include <c64/input.h>
-#include <audio.h>
-#else
+					#ifdef __NES__
+						#pragma rodata-name ("RODATA")
+						#include <nes/core.h>
+						#include <nes/video.h>
+						#include <nes/input.h>
+						#include <audio.h>
+					#endif // #ifdef __NES__
 
-#ifdef __NES__
+				#endif // #ifdef __C64__
+			#endif // #ifdef __CC65__
 
-#pragma rodata-name ("RODATA")
-#include <nes/core.h>
-#include <nes/video.h>
-#include <nes/input.h>
-#include <audio.h>
-#endif
+			#ifdef __WATCOMC__
+				#define HAS_KEYBOARD
 
-#endif
+				#include <msdos/core.h>
+				#include <msdos/video.h>
+				#include <msdos/color.h>
+				#include <msdos/input.h>
+				#include <msdos/audio.h>
+			#else
 
-#ifdef __WATCOMC__
-#define HAS_KEYBOARD
+				#ifdef __NDS__
+					#include <nds/core.h>
+					#include <nds/video.h>
+					#include <nds/color.h>
+					#include <nds/input.h>
+					#include <nds/audio.h>
+				#else
 
-#include <msdos/core.h>
-#include <msdos/video.h>
-#include <msdos/color.h>
-#include <msdos/input.h>
-#include <msdos/audio.h>
-#else
-
-#ifdef __NDS__
-#include <nds/core.h>
-#include <nds/video.h>
-#include <nds/color.h>
-#include <nds/input.h>
-#include <nds/audio.h>
-#else
-
-#endif
-#endif
-#endif
-#endif
-#endif
-#endif
+				#endif // #ifdef __NDS__
+			#endif // #ifdef __WATCOMC__
+		#endif // #if defined(__linux__)...
+	#endif // #if defined(__NULL__)
+#endif // #ifndef PUZL_H
